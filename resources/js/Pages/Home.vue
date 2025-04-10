b1dad8b9c3c6c442c87f91cae8dc69f3be9953a5
Victor <alisw551@gmail.com>
2025-04-10 22:50:00 +0800

    新增首頁

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8">
+        <h1 class="text-3xl font-bold mb-6">物品管理系統</h1>
+        <p class="text-gray-700 mb-8">歡迎使用您的專屬物品管理工具。</p>
+
+        <router-link
+            to="/items"
+            class="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        >
+            開始管理物品
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+</script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import Home from './Pages/Home.vue'
 import ItemManager from './Pages/ItemManager.vue'
 import CreateItem from './Pages/CreateItem.vue'
 import ItemView from './Pages/ItemView.vue'
@@ -7,12 +8,29 @@ import EditItem from './Pages/EditItem.vue'
 const routes = [
     {
         path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/items',
         name: 'ItemList',
         component: ItemManager
     },
-    {path: '/create', component: CreateItem},
-    {path: '/items/:id', component: ItemView},
-    { path: '/edit/:id', component: EditItem },
+    {
+        path: '/items/create',
+        name: 'ItemCreate',
+        component: CreateItem
+    },
+    {
+        path: '/items/:id',
+        name: 'ItemView',
+        component: ItemView
+    },
+    {
+        path: '/items/:id/edit',
+        name: 'ItemEdit',
+        component: EditItem
+    },
 ]
 
 export default createRouter({
